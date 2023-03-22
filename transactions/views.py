15e5282fb9240060227ae034bc1f7f0d1bb4bd54
amrrrr20 <127718621+amrrrr20@users.noreply.github.com>
2023-03-22 22:11:41 +0530

    Add files via upload

---
+++ transactions/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+class DepositMoneyView:
+    pass
+
+
+class WithdrawMoneyView:
+    pass
+
+
+class TransactionRepostView:
+    pass
